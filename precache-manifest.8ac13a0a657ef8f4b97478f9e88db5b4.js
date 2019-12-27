161e340b4cd6b974b954d8bb98a288cdedbd3447
sonnguyen97 <nguyencongson163@gmail.com>
2019-12-28 02:38:43 +0700

    Add files via upload

---
+++ precache-manifest.8ac13a0a657ef8f4b97478f9e88db5b4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "93d3bbba82d64f61aa68",
+    "url": "/static/css/4.68f5989b.chunk.css"
+  },
+  {
+    "revision": "eb4a491a13cad557c5ec",
+    "url": "/static/css/0.144f4d7e.chunk.css"
+  },
+  {
+    "revision": "1ac157e5f8ac3bbcdb29",
+    "url": "/static/css/main.cc236c1e.chunk.css"
+  },
+  {
+    "revision": "1ac157e5f8ac3bbcdb29",
+    "url": "/static/js/main.c1f60380.chunk.js"
+  },
+  {
+    "revision": "3256570410975f890af8",
+    "url": "/static/js/runtime~main.e8336443.js"
+  },
+  {
+    "revision": "f6d7a7b9612d68c1fee6",
+    "url": "/static/css/3.49110d6a.chunk.css"
+  },
+  {
+    "revision": "f6d7a7b9612d68c1fee6",
+    "url": "/static/js/3.31c9ef51.chunk.js"
+  },
+  {
+    "revision": "eb4a491a13cad557c5ec",
+    "url": "/static/js/0.dc551ee0.chunk.js"
+  },
+  {
+    "revision": "93d3bbba82d64f61aa68",
+    "url": "/static/js/4.3e3b37d9.chunk.js"
+  },
+  {
+    "revision": "f01dd7efc9885df4bf1d",
+    "url": "/static/js/5.54644bc6.chunk.js"
+  },
+  {
+    "revision": "999184549f63b22a2f1f",
+    "url": "/static/css/6.b6adedb1.chunk.css"
+  },
+  {
+    "revision": "999184549f63b22a2f1f",
+    "url": "/static/js/6.a8b66610.chunk.js"
+  },
+  {
+    "revision": "e9a215de1bcc5195a247",
+    "url": "/static/js/7.72314fc7.chunk.js"
+  },
+  {
+    "revision": "93e804256ecb97a45296",
+    "url": "/static/js/8.c9b687ff.chunk.js"
+  },
+  {
+    "revision": "ddf9ed948a9fcdc4511b8cd1784e81ec",
+    "url": "/index.html"
+  }
+];
